f2844ac12e58ae5c1ba90678ae6742c4d59d4ac1
Pavel Nosov <soundrussian@gmail.com>
2014-10-19 00:57:20 +0400

    Составитель-класс

--- app/widgets/LatestNews.php
+++ app/widgets/LatestNews.php
@@ -1,5 +1,11 @@
 <?php
 
-View::composer('_latest_news', function($view) {
-  $view->with('latest_news', News::latest()->get());
-});
+class LatestNews
+{
+    public function compose($view)
+    {
+        $view->with('latest_news', News::latest()->get());
+    }
+}
+
+View::composer('_latest_news', 'LatestNews');
